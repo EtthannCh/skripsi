353624a1e8d55b4e4d1cb1e62e10554a557f4018
etthannch <etthannch@gmail.com>
2025-03-23 11:09:48 +0700

    cp - intergrate resend with application

--- src/routes/forgot-password/+page.server.ts
+++ src/routes/forgot-password/+page.server.ts
@@ -1,11 +1,11 @@
+import { RESEND_API_KEY } from "$env/static/private";
+import { OtpSessionManager } from "$lib/server/sessionManager";
 import { fail, redirect } from "@sveltejs/kit";
+import { Resend } from "resend";
 import { superValidate } from "sveltekit-superforms";
 import { zod } from "sveltekit-superforms/adapters";
 import type { Actions } from "./$types";
 import { forgotPasswordSchema } from "./forgot-password-schema";
-import { OtpSessionManager } from "$lib/server/sessionManager";
-import { GOOGLE_EMAIL, GOOGLE_PASSWORD } from "$env/static/private";
-import nodemailer from "nodemailer";
 
 export const load = async () => {
     const form = await superValidate(zod(forgotPasswordSchema))
@@ -18,7 +18,7 @@ export const actions: Actions = {
     forgot: async ({ request, cookies }) => {
         const form = await superValidate(request, zod(forgotPasswordSchema));
         if (!form.valid) {
-            return fail(400, { data: form})
+            return fail(400, { data: form })
         }
 
         const otp: number = Math.floor(Math.random() * (999999 - 100000) + 100000);
@@ -32,32 +32,15 @@ export const actions: Actions = {
         }
 
         // ganti email dengan email dari form
-        sendEmail("kelvinrogue6@gmail.com", "Reset Password OTP Code", `Complete Your Reset Password Process. \n OTP : ${otp}`);
+        const resend = new Resend(RESEND_API_KEY);
+        const response = await resend.emails.send({
+            from: "no-reply@uph-academic-services.web.id",
+            to: "kelvinrogue6@gmail.com",
+            subject: "Reset Password OTP Code",
+            html: `<p>Complete Your Reset Password Process. \n OTP : ${otp}</p>`
+        })
+        console.log(response.error);
+
         return redirect(303, "/forgot-password/verify")
-    }
-}
-
-const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
-    auth: {
-        user: GOOGLE_EMAIL,
-        pass: GOOGLE_PASSWORD
-    }
-})
-
-const sendEmail = async (to: string, subject: string, text: string) => {
-    const mailOptions = {
-        from: GOOGLE_EMAIL,
-        to,
-        subject,
-        text
-    }
-    try {
-        const info = await transporter.sendMail(mailOptions);
-        console.log("Email Sent", info.response);
-    } catch (error) {
-        console.log(error);
     }
 }
